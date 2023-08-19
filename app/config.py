915d82cd2cc8c83a6f355eddae429537431ee6a3
pip-uninstall <privalov_nikita@icloud.com>
2023-08-19 18:05:17 +0300

    connect mysql, update files, add links to posts pages

--- app/config.py
+++ app/config.py
@@ -1,5 +1,7 @@
 class Configuration(object):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:134893@MySQL@localhost/test1"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:134893_MySQL@localhost/test1'
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///blog.db'
 
 
