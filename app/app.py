a3977c2fcaea7cc65e13801c88df277c15795684
pip-uninstall <privalov_nikita@icloud.com>
2023-08-24 14:13:05 +0300

    add admin panel, add form edit post, add pagination

--- app/app.py
+++ app/app.py
@@ -3,6 +3,9 @@ from config import Configuration
 from flask_sqlalchemy import SQLAlchemy
 
 from flask_migrate import Migrate
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
 
 app = Flask(__name__)
 
@@ -12,3 +15,8 @@ db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
 
+###ADMIN###
+from models import *
+admin = Admin(app)
+admin.add_view(ModelView(Post, db.session))
+admin.add_view(ModelView(Tag, db.session))
--- app/config.py
+++ app/config.py
@@ -2,6 +2,6 @@ class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:134893_MySQL@localhost/test1'
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///blog.db'
+    SECRET_KEY = 'something secret'
 
 
